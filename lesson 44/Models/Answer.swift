069f82a6d3ea99214c6bfac2a405c5bb0db94ff7
sergeiKalinin35 <piolersa85@mail.ru>
2022-11-28 23:01:27 +0200

    Question

---
+++ lesson 44/Models/Answer.swift
@@ -0,0 +1,8 @@
+//
+//  Answer.swift
+//  lesson 44
+//
+//  Created by Sergey on 28.11.2022.
+//
+
+import Foundation
---
+++ lesson 44/Models/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  lesson 44
+//
+//  Created by Sergey on 28.11.2022.
+//
+
+import Foundation
