97550e7149506af09c37e0ee5caab7e438e6351b
sergeiKalinin35 <piolersa85@mail.ru>
2022-11-28 23:41:33 +0200

    questions

--- lesson 44/Models/Question.swift
+++ lesson 44/Models/Question.swift
@@ -18,8 +18,43 @@ struct Question {
     let type: ResponseType
     let answers: [Answer]
 }
-
-
+// Так как нет базы данных, мы обходим правило и hardcoding свои вопросы, а не берем их с сети json.
+extension Question {
+    static func getQuestions() -> [Question] {
+        return [
+            Question(
+                text: "Какую пищу вы предпочитаете?",
+                type: .single,
+                answers: [
+                    Answer(text: "Стейк", type: .dog),
+                    Answer(text: "Рыба", type: .cat),
+                    Answer(text: "Морковь", type: .rabbit),
+                    Answer(text: "Кукуруза", type: .turtle)
+                ]
+            ),
+            Question(
+                text: "Что вам нравится больше?",
+                type: .multiple,
+                answers: [
+                    Answer(text: "Плавать", type: .dog),
+                    Answer(text: "Спать", type: .cat),
+                    Answer(text: "Сидеть", type: .rabbit),
+                    Answer(text: "Есть", type: .turtle)
+                ]
+            ),
+            Question(
+                text: "Любите ли вы поездки на машине?",
+                type: .ranged,
+                answers: [
+                    Answer(text: "Счастлив", type: .dog),
+                    Answer(text: "Ненавижу", type: .cat),
+                    Answer(text: "Нервничаю", type: .rabbit),
+                    Answer(text: "Обожаю", type: .turtle)
+                ]
+            )
+        ]
+    }
+}
 
 
 
