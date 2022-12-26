5ebba931db33e1eab06d0a2391dbc5ed8c05fd39
sergeiKalinin35 <piolersa85@mail.ru>
2022-12-26 23:29:32 +0200

    slider

--- lesson 44/View Controllers/QuestionsViewController.swift
+++ lesson 44/View Controllers/QuestionsViewController.swift
@@ -8,22 +8,16 @@
 import UIKit
 
 class QuestionsViewController: UIViewController {
-
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var progressView: UIProgressView!
+    @IBOutlet var rangedSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
